2c0c7d10618674ca481978720e1c1c056066b70a
Janek Brandt <brandt.janek@gmail.com>
2018-02-19 18:14:06 -0800

    add simple command line prompt to shuffle the deck and draw a card

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,32 @@
+import java.io.Console;
+
 public class App {
     public static void main(String[] args) {
         Deck deck = new Deck();
         deck.shuffleDeck();
-        for (int i = 0; i < 52; i++) {
-            System.out.println(deck.drawCard());
+
+        Console console = System.console();
+        boolean running = true;
+
+        System.out.println("Type S to shuffle the deck");
+        System.out.println("Type D to draw a card from the deck");
+        System.out.println("Type E to exit");
+
+        while (running) {
+            String userInput = console.readLine();
+
+            if (userInput.equals("S")) {
+                deck.shuffleDeck();
+            }
+
+            if (userInput.equals("D")) {
+                System.out.println(deck.drawCard());
+            }
+
+            if (userInput.equals("E")) {
+                running = false;
+            }
+            
         }
         
     }
